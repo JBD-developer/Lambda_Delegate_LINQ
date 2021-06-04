21ed764f909e13b9698e345b9054bcc010ba2da1
JBD <lio222@hanmail.net>
2021-06-04 09:23:16 +0900

    2021-06-04 Lambda Expression

--- Lambda001/Lambda001/Program.cs
+++ Lambda001/Lambda001/Program.cs
@@ -9,12 +9,22 @@ namespace Lambda001
     class Program
     {
         delegate int Calculate(int p_a, int p_b);
-
+        delegate void DoShoething();
         static void Main(string[] args)
         {
             Calculate calc = (a, b) => a + b;
 
             Console.WriteLine($"{3} + {4} : {calc(3, 4)}");
+
+            DoShoething DoIt = () =>
+            {
+                Console.WriteLine("Lambda Test");
+                Console.WriteLine("Lambda Test01");
+                Console.WriteLine("Lambda Test02");
+            };
+
+            DoIt();
+        
         }
     }
 }

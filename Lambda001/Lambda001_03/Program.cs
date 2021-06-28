ec393d6af27b18c3fa1a1cff486f66cd8a28dcf9
JBD <lio222@hanmail.net>
2021-06-28 19:08:20 +0900

    2021-06-28 Lambda Func

---
+++ Lambda001/Lambda001_03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda001_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using Func 
+            Func<int> func1 = () => 10;
+            Console.WriteLine($"func1() : {func1()}");
+
+            Func<int, int> func2 = (x) => x * 2;
+            Console.WriteLine($"func2() : {func2(3)}");
+
+            Func<double, double, double> func3 = (x, y) => x/y;
+            Console.WriteLine($"func3() : {func3(4, 5)}");
+            
+            Console.ReadLine();
+        }
+    }
+}

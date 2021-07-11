bd4d8695d7c84fb907f5f4578f07160eb3d350de
JBD <lio222@hanmail.net>
2021-07-11 16:48:43 +0900

    2021-07-11 Delegate

---
+++ Delegate001/Delegate001_02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate001_02
+{
+    //Delegate 
+    delegate int delMyDelegate(string p_s);
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private void LF_Test()
+        {
+            // Delegate 
+            delMyDelegate del = new delMyDelegate(LF_StringToParse);
+            
+            // Delegate Object 
+            LF_DelExecution(del);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p_s"></param>
+        /// <returns></returns>
+        private int LF_StringToParse(string p_s)
+        {
+            Console.WriteLine(p_s.GetType().ToString());
+            return int.Parse(p_s);
+        }
+
+        /// <summary>
+        /// Delegate를 
+        /// </summary>
+        /// <param name="p_del"></param>
+        private void LF_DelExecution(delMyDelegate p_del)
+        {
+            int i = p_del("123");
+            Console.WriteLine(i);
+            Console.WriteLine(i.GetType().ToString());
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            new Program().LF_Test();
+        }
+    }
+}
---
+++ Delegate001/Delegate001_03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate001_03
+{
+    class RunTest
+    {
+        private delegate void delegateTest(int i);
+
+        private void LF_DelThis(int p_iVal)
+        {
+            Console.WriteLine($"LF_Delthis : {p_iVal}");
+        }
+
+        private void LF_DelThat(int p_iVal2)
+        {
+            Console.WriteLine("LF_DelThat : 0x{0:X}", p_iVal2);
+            Console.ReadLine();
+        }
+
+        public void LF_DelExecution()
+        {
+            delegateTest del = new delegateTest(LF_DelThis);
+            del(1024);
+
+            del = LF_DelThat;
+            del(1034);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTest test = new RunTest();
+            test.LF_DelExecution();
+        }
+    }
+}

5fa90dd0aec9cb0e3606788e8273696a20123acc
JBD <lio222@hanmail.net>
2021-07-13 08:11:35 +0900

    2021-07-13 Event

---
+++ Event001/Event001_001/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Event001_001
+{
+    
+    public class Metronome
+    {
+        public event TickHandler Tick;
+        public delegate void TickHandler(Metronome m, EventArgs e);
+        public EventArgs e = null;
+        
+        public void LF_Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                if (Tick != null)
+                {
+                    Tick(this, e);
+                }
+            }
+        }
+    }
+    
+    public class Listener
+    {
+        public void LF_Subcribe(Metronome m)
+        {
+            m.Tick += new Metronome.TickHandler(LF_HeardIt);
+        }
+
+        private void LF_HeardIt(Metronome m, EventArgs e)
+        {
+            Console.WriteLine("Heard It");
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Metronome metronome = new Metronome();
+            Listener listener = new Listener();
+            listener.LF_Subcribe(metronome);
+            metronome.LF_Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Event001/Event001_002/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Event001_002
+{
+    public class TimeOfTock : EventArgs
+    {
+        private DateTime TimeNow;
+
+        public DateTime Time
+        {
+            get
+            {
+                return this.TimeNow;
+            }
+
+            set
+            {
+                TimeNow = value;
+            }
+        }
+    
+    }
+
+    public class Metronome
+    {
+        public event TickHandler Tick;
+        public delegate void TickHandler(Metronome m, TimeOfTock e);
+
+        public void LF_Start()
+        {
+
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                
+                if (Tick != null)
+                {
+                    TimeOfTock tot = new TimeOfTock();
+                    tot.Time = DateTime.Now;
+                    Tick(this, tot);
+                }
+            }
+            
+        }
+    }
+
+    public class Listener
+    {
+        public void LF_Subscribe(Metronome m)
+        {
+            m.Tick += new Metronome.TickHandler(LF_HeardIt);
+        }
+
+        private void LF_HeardIt(Metronome m, TimeOfTock e)
+        {
+            Console.WriteLine($"HEARD IT AT {e.Time}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metronome = new Metronome();
+            var listener = new Listener();
+            listener.LF_Subscribe(metronome);
+            metronome.LF_Start();
+        }
+    }
+}

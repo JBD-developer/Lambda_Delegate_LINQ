c77b9c5cc85546551f3a2b242065279288fe1af7
JBD <lio222@hanmail.net>
2021-07-16 20:33:59 +0900

    2021-07-16 Event

---
+++ Event001/Event001_003/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event001_003
+{
+    public class ButtonClass
+    {
+        public delegate void EventHandler();
+        public event EventHandler Click; 
+
+        public void LF_OnClick()
+        {
+            if (Click != null)
+            {
+                Click();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ButtonClass btn = new ButtonClass();
+
+            btn.Click += new ButtonClass.EventHandler(LF_Print);
+            btn.Click += new ButtonClass.EventHandler(LF_Print);
+
+            btn.LF_OnClick();
+            Console.ReadLine();
+        }
+        static void LF_Print()
+        {
+            Console.WriteLine("Event Print");
+        }
+    }
+}
---
+++ Event001/Event001_004/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event001_004
+{
+    delegate void EventHandler(string messsage); 
+
+    class MyNotifier
+    {
+        public event EventHandler SomeThingHappend;
+        public void LF_DoSomeThing(int number)
+        {
+            int temp = number % 10;
+
+            if (temp !=0 && temp% 3 == 0)
+            {
+                SomeThingHappend(string.Format("{0} : 짝", number));
+            }
+        }
+    }
+    class Program
+    {
+        public static void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomeThingHappend += new EventHandler(MyHandler);
+
+            for (int i = 0; i < 30; i++)
+            {
+                notifier.LF_DoSomeThing(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
